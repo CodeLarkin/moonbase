d2a4747b313818a7ceb54aaf4f0b4b5dda866168
Larkin <code.larkin@gmail.com>
2023-08-03 15:01:46 -0400

    compiling MoonbaseOGCaptain and minting, can't get tokenURI

--- scripts/nft-grid.sh
+++ scripts/nft-grid.sh
@@ -1,5 +1,12 @@
 set -e
 
-~/.foundry/bin/forge test -vv
-python3 scripts/domeUrisFromMeta.py 'gen/domeURIs.json' > gen/domeURIs.html
-brave-browser gen/domeURIs.html
+# first run:
+#~/.foundry/bin/forge test -vv
+
+    python3 scripts/domeUrisFromMeta.py 'gen/domeURIs.json' > gen/domeURIs.html
+    brave-browser gen/domeURIs.html
+fi
+if [ "$1" == captains ]; then
+    python3 scripts/domeUrisFromMeta.py 'gen/captainURIs.json' > gen/captainURIs.html
+    brave-browser gen/domeURIs.html
+fi
--- test/MoonbaseOGCaptain.t.sol
+++ test/MoonbaseOGCaptain.t.sol
@@ -2,19 +2,18 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {MoonbaseOGCaptain} from "../src/MoonbaseOGCaptain.sol";
+import {MoonbaseCaptainURI} from "../src/MoonbaseOGCaptainSimpleURI.sol";
+import {IVisualizer, MoonbaseOGCaptain} from "../src/MoonbaseOGCaptainSimple.sol";
 
-contract MoonbaseOGCaptainTest is Test {
+contract MoonbaseOGCaptainSimpleTest is Test {
+    //IVisualizer public visualizer;
+    MoonbaseCaptainURI public visualizer;
     MoonbaseOGCaptain public captains;
     address public owner;
-    uint256 constant public NUM_DOMES = 100;
+    uint256 constant public NUM_NFTS = 100;
+    uint256 constant public FIRST_TOKEN_ID = 1;
 
     // HELPER FUNCTIONS
-    function getCost(uint tokenId) public view returns (uint) {
-        uint quality = captains.getFarmQuality(tokenId);
-        return captains.costs(quality);
-    }
-
     function setSenderAndFund(address sender, uint256 amount) public {
         vm.prank(sender);
         vm.deal(sender, amount);
@@ -22,24 +21,26 @@ contract MoonbaseOGCaptainTest is Test {
 
     // TEST SETUP
     function setUp() public {
-        captains = new MoonbaseOGCaptain();
+        visualizer = new MoonbaseCaptainURI();
+        captains = new MoonbaseOGCaptain(address(visualizer));
         owner = vm.addr(1);
     }
 
     // TESTS
     function testMint() public {
-        string[] memory uris = new string[](NUM_DOMES);
-        for (uint256 tokenId = 0; tokenId < NUM_DOMES; tokenId++) {
+        string[] memory uris = new string[](NUM_NFTS);
+        for (uint256 tokenId = FIRST_TOKEN_ID; tokenId < NUM_NFTS + FIRST_TOKEN_ID; tokenId++) {
+            console2.log("tokenId: %s", tokenId);
 
-            uint cost = getCost(tokenId);
-            setSenderAndFund(owner, cost);
-            captains.create{value: cost}(tokenId);
+            setSenderAndFund(owner, 1 ether);
+            string memory name = string.concat("Captain ", vm.toString(tokenId));
+            captains.mint(name, 5, 5, 5);
 
             assertEq(captains.ownerOf(tokenId), owner);
 
-            uris[tokenId] = captains.tokenURI(tokenId);
+            uris[tokenId] = captains.tokenURI(tokenId); // FIXME(dbanks12): failing here!
         }
         string memory urisJson = vm.serializeString("", "uris", uris);
-        vm.writeJson(urisJson, "./gen/domeURIs.json");
+        vm.writeJson(urisJson, "./gen/captainURIs.json");
     }
 }
