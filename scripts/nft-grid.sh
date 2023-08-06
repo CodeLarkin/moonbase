3b1d7d092e447d15e275f17f05bf07b64908374a
Larkin <code.larkin@gmail.com>
2023-08-06 15:21:23 -0400

    firefox over brave

--- scripts/nft-grid.sh
+++ scripts/nft-grid.sh
@@ -6,9 +6,9 @@ set -e
 
 if [[ "$1" == domes ]]; then
     python3 scripts/domeUrisFromMeta.py 'gen/domeURIs.json' > gen/domeURIs.html
-    brave-browser gen/domeURIs.html
+    firefox gen/domeURIs.html
 fi
 if [[ "$1" == captains ]]; then
     python3 scripts/domeUrisFromMeta.py 'gen/captainURIs.json' > gen/captainURIs.html
-    brave-browser gen/captainURIs.html
+    firefox gen/captainURIs.html
 fi
